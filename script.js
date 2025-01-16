a2406a1feb7326afdabde54e8298ae24402bbf0f
Yana <sxxqvs@gmail.com>
2025-01-16 17:00:57 +0200

    init

---
+++ script.js
@@ -0,0 +1,158 @@
+function random() {
+    let randomNum = Math.floor(Math.random() * 6) + 1;
+    return randomNum
+}
+
+
+function randomCube1() {
+    let rnd = random();
+    if(rnd === 1) {
+        // document.querySelector("#circleOne").classList.add("hidden");
+        // document.querySelector("#circleTwo").classList.add("hidden");
+        // document.querySelector("#circleThree").classList.add("hidden");
+        // document.querySelector("#circleFour").classList.add("hidden");
+        document.querySelector("#circleFive").classList.add("hidden");
+        // document.querySelector("#circleSix").classList.add("hidden");
+        // document.querySelector("#circleSeven").classList.add("hidden");
+        // document.querySelector("#circleEight").classList.add("hidden");
+        // document.querySelector("#circleNine").classList.add("hidden");
+    } else if(rnd === 2) {
+        document.querySelector("#circleOne").classList.add("hidden");
+        // document.querySelector("#circleTwo").classList.add("hidden");
+        // document.querySelector("#circleThree").classList.add("hidden");
+        // document.querySelector("#circleFour").classList.add("hidden");
+        // document.querySelector("#circleFive").classList.add("hidden");
+        // document.querySelector("#circleSix").classList.add("hidden");
+        // document.querySelector("#circleSeven").classList.add("hidden");
+        // document.querySelector("#circleEight").classList.add("hidden");
+        document.querySelector("#circleNine").classList.add("hidden");
+    } else if(rnd === 3) {
+        document.querySelector("#circleOne").classList.add("hidden");
+        // document.querySelector("#circleTwo").classList.add("hidden");
+        // document.querySelector("#circleThree").classList.add("hidden");
+        // document.querySelector("#circleFour").classList.add("hidden");
+        document.querySelector("#circleFive").classList.add("hidden");
+        // document.querySelector("#circleSix").classList.add("hidden");
+        // document.querySelector("#circleSeven").classList.add("hidden");
+        // document.querySelector("#circleEight").classList.add("hidden");
+        document.querySelector("#circleNine").classList.add("hidden");
+    } else if(rnd === 4) {
+        document.querySelector("#circleOne").classList.add("hidden");
+        // document.querySelector("#circleTwo").classList.add("hidden");
+        document.querySelector("#circleThree").classList.add("hidden");
+        // document.querySelector("#circleFour").classList.add("hidden");
+        // document.querySelector("#circleFive").classList.add("hidden");
+        // document.querySelector("#circleSix").classList.add("hidden");
+        document.querySelector("#circleSeven").classList.add("hidden");
+        // document.querySelector("#circleEight").classList.add("hidden");
+        document.querySelector("#circleNine").classList.add("hidden");
+    } else if(rnd === 5) {
+        document.querySelector("#circleOne").classList.add("hidden");
+        // document.querySelector("#circleTwo").classList.add("hidden");
+        document.querySelector("#circleThree").classList.add("hidden");
+        // document.querySelector("#circleFour").classList.add("hidden");
+        document.querySelector("#circleFive").classList.add("hidden");
+        // document.querySelector("#circleSix").classList.add("hidden");
+        document.querySelector("#circleSeven").classList.add("hidden");
+        // document.querySelector("#circleEight").classList.add("hidden");
+        document.querySelector("#circleNine").classList.add("hidden");
+    } else {
+        document.querySelector("#circleOne").classList.add("hidden");
+        document.querySelector("#circleTwo").classList.add("hidden");
+        document.querySelector("#circleThree").classList.add("hidden");
+        // document.querySelector("#circleFour").classList.add("hidden");
+        // document.querySelector("#circleFive").classList.add("hidden");
+        // document.querySelector("#circleSix").classList.add("hidden");
+        document.querySelector("#circleSeven").classList.add("hidden");
+        document.querySelector("#circleEight").classList.add("hidden");
+        document.querySelector("#circleNine").classList.add("hidden");
+    }
+    return rnd
+}
+
+
+function randomCube2() {
+    let rnd = random();
+    if(rnd === 1) {
+        // document.querySelector("#circleOne").classList.add("hidden");
+        // document.querySelector("#circleTwo").classList.add("hidden");
+        // document.querySelector("#circleThree").classList.add("hidden");
+        // document.querySelector("#circleFour").classList.add("hidden");
+        document.querySelector("#circleFive5").classList.add("hidden");
+        // document.querySelector("#circleSix").classList.add("hidden");
+        // document.querySelector("#circleSeven").classList.add("hidden");
+        // document.querySelector("#circleEight").classList.add("hidden");
+        // document.querySelector("#circleNine").classList.add("hidden");
+    } else if(rnd === 2) {
+        document.querySelector("#circleOne1").classList.add("hidden");
+        // document.querySelector("#circleTwo").classList.add("hidden");
+        // document.querySelector("#circleThree").classList.add("hidden");
+        // document.querySelector("#circleFour").classList.add("hidden");
+        // document.querySelector("#circleFive").classList.add("hidden");
+        // document.querySelector("#circleSix").classList.add("hidden");
+        // document.querySelector("#circleSeven").classList.add("hidden");
+        // document.querySelector("#circleEight").classList.add("hidden");
+        document.querySelector("#circleNine9").classList.add("hidden");
+    } else if(rnd === 3) {
+        document.querySelector("#circleOne1").classList.add("hidden");
+        // document.querySelector("#circleTwo").classList.add("hidden");
+        // document.querySelector("#circleThree").classList.add("hidden");
+        // document.querySelector("#circleFour").classList.add("hidden");
+        document.querySelector("#circleFive5").classList.add("hidden");
+        // document.querySelector("#circleSix").classList.add("hidden");
+        // document.querySelector("#circleSeven").classList.add("hidden");
+        // document.querySelector("#circleEight").classList.add("hidden");
+        document.querySelector("#circleNine9").classList.add("hidden");
+    } else if(rnd === 4) {
+        document.querySelector("#circleOne1").classList.add("hidden");
+        // document.querySelector("#circleTwo").classList.add("hidden");
+        document.querySelector("#circleThree3").classList.add("hidden");
+        // document.querySelector("#circleFour").classList.add("hidden");
+        // document.querySelector("#circleFive").classList.add("hidden");
+        // document.querySelector("#circleSix").classList.add("hidden");
+        document.querySelector("#circleSeven7").classList.add("hidden");
+        // document.querySelector("#circleEight").classList.add("hidden");
+        document.querySelector("#circleNine9").classList.add("hidden");
+    } else if(rnd === 5) {
+        document.querySelector("#circleOne1").classList.add("hidden");
+        // document.querySelector("#circleTwo").classList.add("hidden");
+        document.querySelector("#circleThree3").classList.add("hidden");
+        // document.querySelector("#circleFour").classList.add("hidden");
+        document.querySelector("#circleFive5").classList.add("hidden");
+        // document.querySelector("#circleSix").classList.add("hidden");
+        document.querySelector("#circleSeven7").classList.add("hidden");
+        // document.querySelector("#circleEight").classList.add("hidden");
+        document.querySelector("#circleNine9").classList.add("hidden");
+    } else {
+        document.querySelector("#circleOne1").classList.add("hidden");
+        document.querySelector("#circleTwo2").classList.add("hidden");
+        document.querySelector("#circleThree3").classList.add("hidden");
+        // document.querySelector("#circleFour").classList.add("hidden");
+        // document.querySelector("#circleFive").classList.add("hidden");
+        // document.querySelector("#circleSix").classList.add("hidden");
+        document.querySelector("#circleSeven7").classList.add("hidden");
+        document.querySelector("#circleEight8").classList.add("hidden");
+        document.querySelector("#circleNine9").classList.add("hidden");
+    }
+    return rnd
+}
+
+const one = randomCube1();
+const two = randomCube2();
+
+function winner(one, two) {
+    if(one > two) {
+        document.querySelector(".winners").innerHTML = "1 игрок выиграл!"
+    } else if(one < two) {
+        document.querySelector(".winners").innerHTML = "2 игрок выиграл!"
+    } else {
+        document.querySelector(".winners").innerHTML = "Ничья!"
+    }
+}
+
+winner(one, two);
+
+
+
+
+
